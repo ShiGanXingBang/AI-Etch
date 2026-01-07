31f16762255da758529c2fd24160e114df369380
SunWang <2292854115@qq.com>
2026-01-07 23:48:29 +0800

    2026.1.7

---
+++ Open_pkl.py
@@ -0,0 +1,12 @@
+# 两种方法都能打开
+import pickle
+import numpy as np
+
+f = open('./featbin.pkl','rb')
+data = pickle.load(f)
+print(data)
+
+# img_path = './train_data.pkl'
+# img_data = np.load(img_path)
+# print(img_data)
+
